31cece94a0fb223e26f416502acd085e0fe6965b
kloping <3474006766@qq.com>
2021-11-22 16:36:55 +0800

    fix bug

--- src/main/java/io/github/kloping/MySpringTool/h1/impl/AutomaticWiringValueImpl.java
+++ src/main/java/io/github/kloping/MySpringTool/h1/impl/AutomaticWiringValueImpl.java
@@ -5,12 +5,12 @@ import io.github.kloping.MySpringTool.interfaces.AutomaticWiringValue;
 import io.github.kloping.MySpringTool.interfaces.component.ContextManager;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class AutomaticWiringValueImpl implements AutomaticWiringValue {
     @Override
     public boolean wiring(Object o, Field field, ContextManager contextManager) throws IllegalAccessException {
         field.setAccessible(true);
-        if (field.get(o) != null) return false;
         if (field.isAnnotationPresent(AutoStand.class)) {
             AutoStand autoStand = field.getDeclaredAnnotation(AutoStand.class);
             Object o1 = null;
@@ -19,6 +19,8 @@ public class AutomaticWiringValueImpl implements AutomaticWiringValue {
             } else o1 = contextManager.getContextEntity(field.getType(), autoStand.id());
             if (o1 != null)
                 field.set(o, o1);
+            if (Modifier.isStatic(field.getModifiers()))
+                field.set(null, o1);
             return true;
         }
         return false;
