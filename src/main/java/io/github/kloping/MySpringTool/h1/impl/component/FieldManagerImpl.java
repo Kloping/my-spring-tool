0c2aa68ce5948c30d0a43b11cbe77d607f5e8a82
HRS-Computer <3474006766@qq.com>
2021-12-31 17:46:09 +0800

    0.3.3

--- src/main/java/io/github/kloping/MySpringTool/h1/impl/component/FieldManagerImpl.java
+++ src/main/java/io/github/kloping/MySpringTool/h1/impl/component/FieldManagerImpl.java
@@ -14,6 +14,9 @@ import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+/**
+ * @author github-kloping
+ */
 public class FieldManagerImpl implements FieldManager {
 
     private AutomaticWiringValue automaticWiringValue;
