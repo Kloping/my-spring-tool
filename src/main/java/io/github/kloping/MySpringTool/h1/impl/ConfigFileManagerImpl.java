57e70aab06d2a63d3220600ff136444ae3aecc68
HRS-Computer <3474006766@qq.com>
2021-12-31 17:55:10 +0800

    0.3.4

--- src/main/java/io/github/kloping/MySpringTool/h1/impl/ConfigFileManagerImpl.java
+++ src/main/java/io/github/kloping/MySpringTool/h1/impl/ConfigFileManagerImpl.java
@@ -5,9 +5,13 @@ import io.github.kloping.MySpringTool.interfaces.component.ConfigFileManager;
 import io.github.kloping.MySpringTool.interfaces.component.ContextManager;
 import io.github.kloping.file.FileUtils;
 import io.github.kloping.object.ObjectUtils;
+import io.github.kloping.serialize.HMLObject;
 
 import java.io.File;
 
+/**
+ * @author github-kloping
+ */
 public class ConfigFileManagerImpl implements ConfigFileManager {
     private ContextManager contextManager;
 
@@ -16,12 +20,32 @@ public class ConfigFileManagerImpl implements ConfigFileManager {
         this.contextManager = contextManager;
     }
 
+    private static final String SUFFIX = ".hml";
+
     @Override
     public void load(String file) {
+        if (!file.endsWith(SUFFIX)) {
+            loadTxt(file);
+        } else {
+            loadHml(file);
+        }
+    }
+
+    private void loadHml(String file) {
+        String string = FileUtils.getStringFromFile(file);
+        HMLObject object = HMLObject.parseObject(string);
+        object.getEntry().forEach((k, v) -> {
+            contextManager.append(v.getClass(), v, k);
+        });
+    }
+
+    private void loadTxt(String file) {
         String[] ss = FileUtils.getStringsFromFile(file);
         for (String s : ss) {
             String[] s2 = s.split("=");
-            if (s2.length < 2) continue;
+            if (s2.length < 2) {
+                continue;
+            }
             String m1 = s.replace(s2[0] + "=", "");
             Object o = ObjectUtils.maybeType(m1.trim());
             contextManager.append(o, s2[0]);
