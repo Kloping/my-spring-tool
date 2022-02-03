cbc1424e19bc5408edc0bfb0791f826cf64c828e
HRS-Computer <3474006766@qq.com>
2022-02-03 13:50:36 +0800

    0.4.2

--- src/main/java/io/github/kloping/MySpringTool/h1/impl/component/HttpClientManagerImpl.java
+++ src/main/java/io/github/kloping/MySpringTool/h1/impl/component/HttpClientManagerImpl.java
@@ -384,6 +384,9 @@ public class HttpClientManagerImpl implements HttpClientManager {
         if (sb.toString().endsWith(AND)) {
             sb.delete(sb.length() - 1, sb.length());
         }
+        if (sb.toString().startsWith(SPLIT)) {
+            sb.delete(0, 1);
+        }
         sb_end.append(DO);
         sb_end.append(sb.toString());
         return sb_end.toString();
